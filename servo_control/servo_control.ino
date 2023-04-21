21e6b3774430bcc46aebd14a08dfb43cddef9dc7
Alexander Boqdzhiev <aleksandar.s.boyadzhiev.2019@elsys-bg.org>
2023-04-21 17:37:27 +0300

    Added Realy Control

--- servo_control/servo_control.ino
+++ servo_control/servo_control.ino
@@ -1,4 +1,5 @@
 #include <Servo.h>
+#include<Keyboard.h>
 int x1;
 int x2;
 int x3;
@@ -11,14 +12,18 @@ int i = 0;
   Servo X;
   Servo Y;
 
+int timer=2;
+const int Realy = 4;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  pinMode(Relay,output);
   X.attach(10);
   Y.attach(11);
   X.write(xpos);
   Y.write(ypos);
+  Keyboard.begin();
 }
 
 void loop() {
@@ -34,7 +39,27 @@ void loop() {
     i++;
     xpos = (100 * x1) + (10* x2) + x3;
     ypos = (100 * y1) + (10* y2) + y3;
+    timer--;
   }
+  if (timer == 0){
+    digitalWrite(Relay, HIGH);
+    delay(1000);
+    digitalWrite(Relay, LOW);
+    delay(1000);
+    timer = 1;
+  }
+
+  if (Serial.available()>0){
+      char inChar  = Serial.read();
+      if (inChar == ' '){
+        digitalWrite(Realy,HIGH);
+        delay(1000);
+        digitalWrite(Relay, LOW);
+        delay(1000);
+      }
+      
+  }
+  
 
   X.write(xpos);
   Y.write(ypos);
